9efe26d57f678421b4698eea3b8aaf7928693bcd
Kowshik Prakasam <kowshik@gmail.com>
2013-01-07 01:00:05 -0800

    Add AllTests for java.

---
+++ java/tests/AllTests.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sorting.MergeSortTest.class, sorting.StupidSortTest.class})
+	
+public class AllTests {
+}
