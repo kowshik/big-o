aeed361320a6bdd4c7d420f9f6fb0372bcd10c05
Kowshik Prakasam <kowshik@gmail.com>
2013-01-07 00:11:46 -0800

    Add junit tests.

--- java/src/sorting/StupidSort.java
+++ java/src/sorting/StupidSort.java
@@ -9,7 +9,7 @@ public class StupidSort {
 	array[bar] = temp;
     }
 
-    public static void stupidSort(int[] array, Comparator<Integer> comparator) {
+    public static int[] stupidSort(int[] array, Comparator<Integer> comparator) {
 	for (int i = 0; i < array.length; i++) {
 	    for (int j = i + 1; j < array.length; j++) {
 		if (comparator.compare(array[i], array[j]) > 0) {
@@ -17,5 +17,7 @@ public class StupidSort {
 		}
 	    } 
 	}
+
+	return array;
     }    
 }
---
+++ java/tests/sorting/StupidSortTest.java
@@ -0,0 +1,53 @@
+package sorting;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class StupidSortTest {
+
+    private final Comparator<Integer> ascOrderComparator = new Comparator<Integer>() {
+	@Override
+	public int compare(Integer a, Integer b) {
+	    return a - b;
+	}
+    };
+
+    private final Comparator<Integer> descOrderComparator = new Comparator<Integer>() {
+	@Override
+	public int compare(Integer a, Integer b) {
+	    return b - a;
+	}
+    };
+
+    @Test
+    public void ascendingOrderSort() {
+	int[] array = {5, 4, 3, 2, 1};
+	int[] sorted = StupidSort.stupidSort(array, ascOrderComparator);
+	assertArrayEquals("Failure => Array returned is not sorted.",
+			  sorted,
+			  new int[]{1, 2, 3, 4, 5});
+    }
+
+    @Test
+    public void descendingOrderSort() {
+	int[] array = {1, 2, 3, 4, 5};
+	int[] sorted = StupidSort.stupidSort(array, descOrderComparator);
+	assertArrayEquals("Failure => Array returned is not sorted.",
+			  sorted,
+			  new int[]{5, 4, 3, 2, 1});
+    }
+
+    @Test
+    public void randomOrderSort() {
+	int[] array = {5, 1, 3, 2, 4};
+	int[] sorted = StupidSort.stupidSort(array, ascOrderComparator);
+	assertArrayEquals("Failure => Array returned is not sorted.",
+			  sorted,
+			  new int[]{1, 2, 3, 4, 5});
+    }
+
+    public static void main(String args[]) {
+	org.junit.runner.JUnitCore.main("sorting.StupidSortTest");
+    }
+}
