2cccee5bfe0832872c0c2009c0313602f1774adc
Kowshik Prakasam <kowshik@gmail.com>
2013-01-07 00:01:17 -0800

    Add junit tests.

---
+++ java/setup.sh
@@ -0,0 +1,1 @@
+export CLASSPATH=$CLASSPATH:`pwd`/lib/junit-4.11.jar:`pwd`/src:`pwd`/tests
--- java/src/sorting/MergeSort.java
+++ java/src/sorting/MergeSort.java
@@ -3,39 +3,6 @@ package sorting;
 import java.util.Comparator;
 
 public class MergeSort {
-    public static void main(String[] args) {
-	Comparator<Integer> ascending = new Comparator<Integer>() {
-	    @Override
-	    public int compare(Integer foo, Integer bar) {
-		return foo - bar;
-	    }
-	};
-
-	int[] array1 = {5, 1, 2, 4, 3};
-	array1 = mergeSort(array1, ascending);
-	for (int x : array1) {
-	    System.out.println(x);
-	}
-	
-	System.out.println("\n");
-
-	int[] array2 = {3, 1, 4, 2};
-	array2 = mergeSort(array2, ascending);
-	for (int x : array2) {
-	    System.out.println(x);
-	}
-
-	System.out.println("\n");
-
-	int[] array3 = {};
-	array3 = mergeSort(array3, ascending);
-	for (int x : array3) {
-	    System.out.println(x);
-	}
-
-	System.out.println("\n");
-    }
-
     public static int[] mergeSort(int[] array, Comparator<Integer> comparator) {
 	if(array.length == 0) {
 	    return new int[]{};
