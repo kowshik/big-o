2873c84f7d926aa23280a89ed83f6246853dd28c
Kowshik Prakasam <kowshik@gmail.com>
2013-01-13 01:07:25 -0800

    [Ruby] Generic heap sort.

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,2 +1,3 @@
 require_relative '../src/sorting/stupid_sort'
 require_relative '../src/sorting/merge_sort'
+require_relative '../src/sorting/heap_sort'
---
+++ ruby/src/sorting/heap_sort.rb
@@ -0,0 +1,70 @@
+module Sorting
+  class << self
+    
+    def heap_sort(array, &comparator)
+      array = heapify_all(array, &comparator)
+      
+      array.length.downto(2) do |length|
+        swap(array, 0, length - 1)
+        array = heapify(array, 0, length - 1, &comparator)
+      end
+
+      array
+    end
+
+    def heapify_all(array, &comparator)
+      parent_index(array.length - 1).downto(0) do |root_index|
+        array = heapify(array, root_index, array.length, &comparator)
+      end
+
+      array
+    end
+
+    def heapify(array, root_index, length = array.length, &comparator)
+      return array if length <= 1
+      
+      index_to_be_swapped = root_index
+      
+      if left_child_index(root_index) < length
+        compared = yield(array[left_child_index(root_index)],
+                         array[index_to_be_swapped])
+        if compared > 0
+          index_to_be_swapped = left_child_index(root_index)
+        end
+      end
+      
+      if right_child_index(root_index) < length
+        compared = yield(array[right_child_index(root_index)],
+                         array[index_to_be_swapped])
+        if compared > 0
+          index_to_be_swapped = right_child_index(root_index)
+        end
+      end
+      
+      if root_index != index_to_be_swapped
+        swap(array, root_index, index_to_be_swapped)
+        array = heapify(array, index_to_be_swapped, length, &comparator)
+      end
+
+      array
+    end
+
+    def parent_index(root_index)
+      ((root_index - 1)/2).floor
+    end
+
+    def left_child_index(root_index)
+      2 * root_index + 1
+    end
+
+    def right_child_index(root_index)
+      2 * root_index + 2
+    end
+    
+    def swap(array, i, j)
+      temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    end
+  end
+end
