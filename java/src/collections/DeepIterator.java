d4a9232adcab45809db36363f161c7ab15689244
Kowshik Prakasam <kowshik@gmail.com>
2013-01-21 00:45:27 -0800

    [Java] Fix javadoc in class DeepIterator.

--- java/src/collections/DeepIterator.java
+++ java/src/collections/DeepIterator.java
@@ -6,26 +6,34 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 /**
- * This class provides a flattening iterator over a Collection<?>. i.e. In its
- * constructor, the class takes in a Collection<?> object which may nest either
- * a Collection<?> object or an object of type T. Also, it is expected that the
- * nested Collection<?> objects will not reference their container Collection<?>
- * objects as this will lead to a loop. But they can nest other Collection<?>
- * objects. The class then iterates through elements in the given Collection<?>
- * object, returning the values in the leaves of the nested structure one-by-one
+ * This class provides a flattening iterator over a Collection<?>.
+ * i.e. In its constructor, the class takes in a Collection<?> object
+ * which may nest either a Collection<?> object or an object of type T.
+ * Also, it is expected that the nested Collection<?> objects will not
+ * reference their container Collection<?> objects as this will lead
+ * to a loop. But they can nest other Collection<?> objects. The class
+ * then iterates through elements in the given Collection<?> object,
+ * returning the values in the leaves of the nested structure one-by-one
  * upon each call to next().
- * 
- * 
- * Example: This deep iterator can be used to iterate over a vector that either
- * contains a vector of integers, or just plain integers, or both. Lets assume
- * that the Collection<?> passed has the following structure:
- * 
- * [int_a, vector_1, int_d, vector_2, int_h] / \ / \ [int_b, int_c] [int_e,
- * vector_3] / / [int_f, int_g]
- * 
- * The iterator returns the leaves in order viz.: int_a, int_b, int_c, int_d,
- * int_e, int_f, int_g, int_h.
+ *
+ *
+ * Example: This deep iterator can be used to iterate over a vector that
+ *          either contains a vector of integers, or just plain integers,
+ *          or both. Lets assume that the Collection<?> passed has the
+ *          following structure:
+ *
+ *                           [int_a, vector_1, int_d, vector_2, int_h]
+ *                                     /                 \
+ *                                    /                   \
+ *                               [int_b, int_c]        [int_e, vector_3]
+ *                                                                /
+ *                                                               /
+ *                                                         [int_f, int_g]
+ *
+ *          The iterator returns the leaves in order viz.: int_a, int_b,
+ *          int_c, int_d, int_e, int_f, int_g, int_h.
  */
+
 public class DeepIterator<T> implements Iterator<T> {
 	// A reference to the item which will be returned during
 	// the next call to next().
