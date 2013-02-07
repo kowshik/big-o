47999fc31610b7371ed16d9c70ed32c7f2279042
Kowshik Prakasam <kowshik@gmail.com>
2013-02-07 00:19:16 -0800

    [Java] # of set bits in a bit array.

---
+++ java/src/common/NumSetBits.java
@@ -0,0 +1,64 @@
+package common;
+
+/**
+ * You have a series of LEDs whose state is represented as a series of bits -- 1
+ * represents ON and 0 represents off. You are also given an offset and a
+ * length. You need to find the number of LED bulbs present in ON state,
+ * starting at the specified offset in the series of bits, until the specified
+ * length from the offset. Write the method signature and code in any language.
+ * (There are a number of tricky cases in low-level bits/bytes manipulation that
+ * I realized only when I started implementing the solution).
+ * 
+ * @author kprakasam
+ * 
+ */
+public class NumSetBits {
+	public static long getNumSetBits(byte[] leds, long offset, long length) {
+		// s/offset/firstBitIndex/ for readability.
+		long firstBitIndex = offset;
+		long lastBitIndex = firstBitIndex + length - 1;
+		byte firstByteIndex = (byte) (firstBitIndex / 8);
+		byte lastByteIndex = (byte) ((firstBitIndex + length - 1) / 8);
+
+		long totalSetBits = 0L;
+		if (firstByteIndex != lastByteIndex) {
+			totalSetBits += getNumSetBitsBetweenWholeBytes(leds,
+					firstByteIndex + 1, lastByteIndex - 1);
+			totalSetBits += getNumSetBits(leds[firstByteIndex],
+					(int) (firstBitIndex % 8), 7);
+			totalSetBits += getNumSetBits(leds[lastByteIndex], 0,
+					(int) (lastBitIndex % 8));
+		} else {
+			totalSetBits += getNumSetBits(leds[firstByteIndex],
+					(int) (firstBitIndex % 8), (int) (lastBitIndex % 8));
+		}
+
+		return totalSetBits;
+	}
+
+	public static int getNumSetBits(byte b, int startOffset, int endOffset) {
+		if ((startOffset < 0 || startOffset > 7)
+				|| (endOffset < 0 || endOffset > 7) || endOffset < startOffset) {
+			throw new IllegalArgumentException(String.format(
+					"Either startOffset: %d or endOffset: %d is incorrect",
+					startOffset, endOffset));
+		}
+
+		int totalBits = 0;
+		for (int bitIndex = startOffset; bitIndex <= endOffset; bitIndex++) {
+			totalBits += (byte) (b & (1 << (8 - bitIndex - 1))) != 0 ? 1 : 0;
+		}
+
+		return totalBits;
+	}
+
+	public static long getNumSetBitsBetweenWholeBytes(byte[] array,
+			int firstByte, int lastByte) {
+		long totalBits = 0;
+		for (int index = firstByte; index <= lastByte; index++) {
+			totalBits += getNumSetBits(array[index], 0, 7);
+		}
+
+		return totalBits;
+	}
+}
---
+++ java/tests/common/NumSetBitsTest.java
@@ -0,0 +1,47 @@
+package common;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NumSetBitsTest {
+	@Test
+	public void getNumSetBits_TestAllBytes() {
+		for (int number = 0; number <= 255; number++) {
+			int expected = 0;
+			for (char c : Integer.toBinaryString(number).toCharArray()) {
+				if (c == '1') {
+					expected++;
+				}
+			}
+
+			assertEquals(expected,
+					NumSetBits.getNumSetBits((byte) number, 0, 7));
+		}
+	}
+
+	@Test
+	public void getNumSetBits_TestOffsetChecking() {
+		assertEquals(8, NumSetBits.getNumSetBits((byte) 255, 0, 7));
+		assertEquals(6, NumSetBits.getNumSetBits((byte) 255, 1, 6));
+		assertEquals(4, NumSetBits.getNumSetBits((byte) 255, 2, 5));
+		assertEquals(2, NumSetBits.getNumSetBits((byte) 255, 3, 4));
+		assertEquals(1, NumSetBits.getNumSetBits((byte) 255, 4, 4));
+	}
+
+	@Test
+	public void getNumSetBits_ByteArray() {
+		byte[] array = new byte[] { (byte) 100, (byte) 255, (byte) 255,
+				(byte) 175 };
+		assertEquals(25, NumSetBits.getNumSetBitsBetweenWholeBytes(array, 0, 3));
+		assertEquals(16, NumSetBits.getNumSetBitsBetweenWholeBytes(array, 1, 2));
+	}
+
+	@Test
+	public void getNumSetBits_ByteArrayWithOffsetAndLength() {
+		byte[] array = new byte[] { (byte) 100, (byte) 255, (byte) 255,
+				(byte) 175 };
+		assertEquals(1, NumSetBits.getNumSetBits(array, 3, 3));
+		assertEquals(20, NumSetBits.getNumSetBits(array, 3, 26));
+	}
+}
