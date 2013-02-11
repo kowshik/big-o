83ab8509dfbcfcabd2a0c0c3316489f2d26e5686
Kowshik Prakasam <kowshik@gmail.com>
2013-02-10 22:34:28 -0800

    [Java] Phone number allotter.

---
+++ java/src/general/PhoneNumberAllotter.java
@@ -0,0 +1,88 @@
+package general;
+
+import common.ArrayUtils;
+
+/**
+ * You are supposed to design an API and a backend for a system that can allot
+ * phone numbers to people living in a city. The API should enable the clients
+ * (people in the city) to do the following:
+ * 
+ * When a client requests for a phone number, it allots one of the available
+ * numbers to them. Some clients may want fancy numbers, so they can
+ * specifically ask for a number to be alloted to them. If the requested number
+ * is available then the system allots it to them, otherwise the system allots
+ * any available number.
+ * 
+ * The system need not have to know which number is alloted to which client. The
+ * same client may make successive requests and get multiple phone numbers for
+ * himself, but the system is not bothered. At any point of time, the system
+ * only knows which phone numbers are alloted and which phone numbers are free.
+ */
+class NoNumbersLeftException extends RuntimeException {
+	private static final long serialVersionUID = -2301472541196529961L;
+
+	public NoNumbersLeftException(String message) {
+		super(message);
+	}
+}
+
+public class PhoneNumberAllotter {
+	private final int size;
+	public int[] phoneNumbers;
+	private int next;
+
+	public PhoneNumberAllotter(int size) {
+		if (size < 0) {
+			throw new IllegalArgumentException(String.format(
+					"Size cannot be negative. You passed: %d", size));
+		}
+		this.size = size;
+		phoneNumbers = new int[size];
+		for (int phoneNumber = 0; phoneNumber < size; phoneNumber++) {
+			phoneNumbers[phoneNumber] = phoneNumber;
+		}
+
+		next = 0;
+	}
+
+	public int get() {
+		if (next == phoneNumbers.length) {
+			throw new NoNumbersLeftException("No phone numbers left.");
+		}
+
+		return phoneNumbers[next++];
+	}
+
+	public int get(int fancyNumber) {
+		if (fancyNumber >= phoneNumbers.length) {
+			throw new IllegalArgumentException(
+					String.format(
+							"Phone numbers requested should be in the range: %d to %d. You passed: %d.",
+							0, size - 1, fancyNumber));
+		}
+
+		if (phoneNumbers[fancyNumber] <= next) {
+			// We cannot allot the fancy number as it is already taken.
+			// So we will allot the next available number;
+			return get();
+		}
+
+		if (phoneNumbers[fancyNumber] == fancyNumber) {
+			// This fancy number's slot marker was never disturbed before, so we
+			// will take it now, but push it into the used window.
+			ArrayUtils.swap(phoneNumbers, fancyNumber, next);
+		} else {
+			// This fancy number's slot marker was disturbed before.
+			// So we will put the previously allotted number at the tip
+			// of the window, and put the tip of the window in the previously
+			// allotted number's slot marker space.
+			int previouslyAllottedNumber = phoneNumbers[fancyNumber];
+			phoneNumbers[fancyNumber] = fancyNumber;
+			phoneNumbers[next] = previouslyAllottedNumber;
+			phoneNumbers[previouslyAllottedNumber] = next;
+		}
+
+		next++;
+		return fancyNumber;
+	}
+}
---
+++ java/tests/general/PhoneNumberAllotterTest.java
@@ -0,0 +1,88 @@
+package general;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PhoneNumberAllotterTest {
+	@Test
+	public void get_RaiseErrorWhenNoNumbersAreLeft() {
+		PhoneNumberAllotter tested = new PhoneNumberAllotter(0);
+
+		boolean thrown = false;
+		try {
+			tested.get();
+		} catch (NoNumbersLeftException e) {
+			thrown = true;
+		}
+
+		assertTrue(thrown);
+	}
+
+	@Test
+	public void get_ReturnExpectedPhoneNumbers() {
+		PhoneNumberAllotter tested = new PhoneNumberAllotter(5);
+
+		assertEquals(0, tested.get());
+		assertEquals(4, tested.get(4));
+		assertEquals(2, tested.get());
+		assertEquals(3, tested.get());
+		assertEquals(1, tested.get());
+	}
+
+	@Test
+	public void get_ReturnSequentialPhoneNumbers() {
+		PhoneNumberAllotter tested = new PhoneNumberAllotter(5);
+
+		assertEquals(0, tested.get());
+		assertEquals(1, tested.get());
+		assertEquals(2, tested.get());
+		assertEquals(3, tested.get());
+		assertEquals(4, tested.get());
+	}
+
+	@Test
+	public void get_FancyNumber_RaiseErrorWhenNoNumbersAreLeft() {
+		PhoneNumberAllotter tested = new PhoneNumberAllotter(1);
+
+		tested.get();
+
+		boolean thrown = false;
+		try {
+			tested.get(0);
+		} catch (NoNumbersLeftException e) {
+			thrown = true;
+		}
+
+		assertTrue(thrown);
+	}
+
+	@Test
+	public void get_FancyNumber_RaiseErrorWhenBadFancyNumberIsPassed() {
+		PhoneNumberAllotter tested = new PhoneNumberAllotter(0);
+
+		boolean thrown = false;
+		try {
+			tested.get(0);
+		} catch (IllegalArgumentException e) {
+			thrown = true;
+		}
+
+		assertTrue(thrown);
+	}
+
+	@Test
+	public void get_FancyNumber_ReturnExpectedFancyNumbers() {
+		PhoneNumberAllotter tested = new PhoneNumberAllotter(8);
+
+		assertEquals(4, tested.get(4));
+		assertEquals(1, tested.get(4));
+		assertEquals(3, tested.get(3));
+		assertEquals(2, tested.get(2));
+		assertEquals(0, tested.get(1));
+		assertEquals(5, tested.get(5));
+		assertEquals(6, tested.get(6));
+		assertEquals(7, tested.get(6));
+	}
+}
