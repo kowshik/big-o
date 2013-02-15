8327bd0d98062c50639a43d7a18ad7f19ec9cc41
Kowshik Prakasam <kowshik@gmail.com>
2013-02-14 17:35:03 -0800

    [Java] Fix comments.

--- java/src/concurrency/BoundedBlockingQueue.java
+++ java/src/concurrency/BoundedBlockingQueue.java
@@ -3,6 +3,27 @@ package concurrency;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Implement add() and remove() methods for the queue data structure.
+ * 
+ * The queue should be a bounded queue i.e. at any point of time, it can only
+ * hold a specified number of elements.
+ * 
+ * The queue should be blocking i.e. if a thread is trying to read from the
+ * queue, and does not find any elements left to read, then it should wait until
+ * an element becomes available. Similarly, if a thread is trying to write to
+ * the queue, and finds the queue to be full, then it should wait until an
+ * element is removed from the queue.
+ * 
+ * The queue should be fair i.e. if there are 10 readers waiting to read from
+ * the queue, and if a queue element becomes available for reading, then the
+ * reader thread that arrived earliest in time should be allowed access to read
+ * the element, and other reader threads should continue to remain blocked.
+ * Similarly, if there are 10 writers waiting to write to the queue, and if
+ * slots become available in the queue for writing, then the writer thread that
+ * arrived earliest in time should be allowed access to write the element, and
+ * the other writer threads should continue to remain blocked.
+ */
 public class BoundedBlockingQueue<T> {
 	private final Queue<T> elementQueue;
 	private final Queue<Long> readerQueue;
