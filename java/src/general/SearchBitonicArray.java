8db516308408cbc2bbe539d489699c6f289aea8b
Kowshik Prakasam <kowshik@gmail.com>
2013-02-17 19:16:42 -0800

    [Java] Search bitonic array.

---
+++ java/src/general/SearchBitonicArray.java
@@ -0,0 +1,70 @@
+package general;
+
+
+/**
+ * Search in a bitonic array. An array is bitonic if it is comprised of an
+ * increasing sequence of integers followed immediately by a decreasing sequence
+ * of integers. Write a program that, given a bitonic array of N distinct
+ * integer values, determines whether a given integer is in the array.
+ * 
+ * Standard version: Use ~3lgN compares in the worst case. Optimal version: Use
+ * ~2lgN compares in the worst case (and prove that no algorithm can guarantee
+ * to perform fewer than ~2lgN compares in the worst case).
+ * 
+ * @author kprakasam
+ * 
+ */
+public class SearchBitonicArray {
+	public static int searchBitonicArray(int[] array, int value) {
+		if (array == null) {
+			return -1;
+		}
+
+		return searchBitonicArray(array, value, 0, array.length - 1);
+	}
+
+	private static int searchBitonicArray(int[] array, int value,
+			int startIndex, int endIndex) {
+		if (startIndex > endIndex) {
+			return -1;
+		}
+
+		int middleIndex = (startIndex + endIndex) / 2;
+		if (value == array[middleIndex]) {
+			return middleIndex;
+		}
+
+		if (array[middleIndex] < array[startIndex]) {
+			//
+			// /\
+			// / \
+			// S \
+			// \ M
+			// \
+			// \
+			// \
+			// \
+			// E
+			//
+			// Here, S => startIndex, M => middleIndex, E => endIndex.
+			//
+			// In the diagram above, by definition of bitonic
+			// array, when array[middleIndex] < array[startIndex],
+			// then array[middleIndex] is always greater than array[endIndex].
+			if (value > array[middleIndex]) {
+				return searchBitonicArray(array, value, startIndex,
+						middleIndex - 1);
+			}
+
+			return searchBitonicArray(array, value, middleIndex + 1, endIndex);
+		}
+
+		int found = searchBitonicArray(array, value, startIndex,
+				middleIndex - 1);
+		if (found == -1) {
+			found = searchBitonicArray(array, value, middleIndex + 1, endIndex);
+		}
+
+		return found;
+	}
+}
