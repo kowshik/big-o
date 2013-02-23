a8736cdd65b7ddb15bf5bf148ae91abc19b5aa49
Kowshik Prakasam <kowshik@gmail.com>
2013-02-22 19:36:31 -0800

    [Java] Add tests.

--- java/tests/AllTests.java
+++ java/tests/AllTests.java
@@ -2,13 +2,16 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ sorting.MergeSortTest.class, sorting.HeapSortTest.class,
-		sorting.QuickSortTest.class, sorting.StupidSortTest.class,
-		sorting.InsertionSortTest.class, sorting.SimpleHeapImplTest.class,
+@Suite.SuiteClasses({ arrays.RearrangeInputToTargetTest.class,
+		binarytree.BstClosestNodeTest.class, binarytree.BstFindModeTest.class,
+		sorting.MergeSortTest.class, cache.LRUCacheTest.class,
 		collections.DeepIteratorTest.class, collections.PeekIteratorTest.class,
 		collections.lists.SinglyLinkedListTest.class,
-		arrays.RearrangeInputToTargetTest.class, common.NumberUtilsTest.class,
-		common.StringUtilsTest.class, general.RainWaterTest.class })
+		common.NumberUtilsTest.class, common.StringUtilsTest.class,
+		general.NumSetBitsTest.class, general.PhoneNumberAllotterTest.class,
+		general.RainWaterTest.class, sorting.HeapSortTest.class,
+		sorting.QuickSortTest.class, sorting.StupidSortTest.class,
+		sorting.InsertionSortTest.class, sorting.SimpleHeapImplTest.class, })
 public class AllTests {
 	public static void main(String[] args) {
 		org.junit.runner.JUnitCore.main("AllTests");
