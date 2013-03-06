21d3eee8a987fa1abc01e97a2ba060b548a8cad3
Kowshik Prakasam <kowshik@gmail.com>
2013-03-05 20:32:54 -0800

    [Java] Find common elements between sorted arrays.

---
+++ java/src/common/FindCommonElements.java
@@ -0,0 +1,30 @@
+package common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Find common elements between two sorted arrays.
+ */
+public class FindCommonElements {
+	public static List<Integer> findCommonElements(int[] a, int[] b) {
+		if (a == null || b == null) {
+			return null;
+		}
+
+		List<Integer> common = new ArrayList<Integer>();
+		for (int aIndex = 0, bIndex = 0; aIndex < a.length && bIndex < b.length;) {
+			if (a[aIndex] < b[bIndex]) {
+				aIndex++;
+			} else if (b[bIndex] < a[aIndex]) {
+				bIndex++;
+			} else {
+				common.add(a[aIndex]);
+				aIndex++;
+				bIndex++;
+			}
+		}
+
+		return common;
+	}
+}
