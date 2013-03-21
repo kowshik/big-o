3502ef894721fef0aa2b41e9d93935dca6d21ddf
Kowshik Prakasam <kowshik@gmail.com>
2013-03-20 19:44:00 -0700

    [Java] Minor fix to comment

--- java/src/arrays/FindNumHops.java
+++ java/src/arrays/FindNumHops.java
@@ -9,7 +9,8 @@ import java.util.Set;
  * (i.e. >= 0) From any index in the array, a hop can be either of the
  * following:
  * 
- * [1] Jump array[index] positions [2] Jump 1 position
+ * [1] Jump array[index] positions
+ * [2] Jump 1 position
  * 
  * The target of this question is to find the number of hops required to reach
  * the last element in the array from the first element.
