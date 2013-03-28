32bd49de292d332f7b5ca01d0468729c276ad0e0
Kowshik Prakasam <kowshik@gmail.com>
2013-03-27 22:48:27 -0700

    [Java] First reader writer problem

---
+++ java/src/concurrency/FirstReaderWriterLock.java
@@ -0,0 +1,83 @@
+package concurrency;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Solves the first reader writer problem:
+ * 
+ * 1. If no write lock has been acquired, then any number of read locks are
+ * allowed to be acquired.
+ * 
+ * 2. If a write lock has been acquired, then read locks are not allowed to be
+ * acquired until the write lock has been released.
+ * 
+ * 3. If one or more read locks have been acquired, then a write lock is not
+ * allowed to be acquired until all read locks have been released.
+ * 
+ * 4. Only threads that previously held a read or write lock are allowed to
+ * release them.
+ * 
+ * Note: Writers can potentially starve if there are many more readers when
+ * compared to writers, but we don't address this in the solution.
+ */
+public class FirstReaderWriterLock {
+	private final Semaphore readMutex = new Semaphore(1);
+	private final Semaphore writeMutex = new Semaphore(1);
+	private final Set<Long> readerIds = Collections
+			.synchronizedSet(new HashSet<Long>());
+	private AtomicLong writerId;
+
+	public void readLock() {
+		try {
+			readMutex.acquire();
+			readerIds.add(Thread.currentThread().getId());
+			if (readerIds.size() == 1) {
+				writeMutex.acquire();
+			}
+		} finally {
+			readMutex.release();
+		}
+	}
+
+	public void readUnlock() {
+		try {
+			readMutex.acquire();
+			long threadId = Thread.currentThread().getId();
+			if (!readerIds.contains(threadId)) {
+				throw new IllegalStateException(
+						String.format(
+								"The current thread with id: %d never acquired a read lock before.",
+								threadId));
+			}
+
+			readerIds.remove(threadId);
+			if (readerIds.size() == 0) {
+				writeMutex.release();
+			}
+		} finally {
+			readMutex.release();
+		}
+
+	}
+
+	public void writeLock() {
+		writeMutex.acquire();
+		writerId.set(Thread.currentThread().getId());
+	}
+
+	public void writeUnlock() {
+		long threadId = Thread.currentThread().getId();
+		synchronized (writerId) {
+			if (writerId.get() != threadId) {
+				throw new IllegalStateException(
+						String.format(
+								"The current thread with id: %d never acquired a write lock before.",
+								threadId));
+			}
+		}
+		writeMutex.release();
+	}
+}
