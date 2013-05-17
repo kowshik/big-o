847638679ac6924e06700ef166b008606ada8637
Kowshik Prakasam <kowshik@gmail.com>
2013-05-16 19:14:53 -0700

    [Java] Add comments

--- java/src/common/ArrayUtils.java
+++ java/src/common/ArrayUtils.java
@@ -3,6 +3,7 @@ package common;
 import java.util.Comparator;
 
 public class ArrayUtils {
+	// Converts an integer array into Integer array.
 	public static Integer[] autoBox(int[] array) {
 		if (array == null) {
 			return null;
@@ -16,6 +17,7 @@ public class ArrayUtils {
 		return converted;
 	}
 
+	// Converts a character array into Character array.
 	public static Character[] autoBox(char[] array) {
 		if (array == null) {
 			return null;
@@ -29,30 +31,37 @@ public class ArrayUtils {
 		return converted;
 	}
 
+	// Swaps elements of a character array.
 	public static void swap(char[] array, int fooIndex, int barIndex) {
 		char temp = array[fooIndex];
 		array[fooIndex] = array[barIndex];
 		array[barIndex] = temp;
 	}
 
+	// Swaps elements of an integer array.
 	public static void swap(int[] array, int fooIndex, int barIndex) {
 		int temp = array[fooIndex];
 		array[fooIndex] = array[barIndex];
 		array[barIndex] = temp;
 	}
 
+	// Swaps elements of a long array.
 	public static void swap(long[] array, int fooIndex, int barIndex) {
 		long temp = array[fooIndex];
 		array[fooIndex] = array[barIndex];
 		array[barIndex] = temp;
 	}
 
+	// Swaps elements of an array.
 	public static <T> void swap(T[] array, int fooIndex, int barIndex) {
 		T temp = array[fooIndex];
 		array[fooIndex] = array[barIndex];
 		array[barIndex] = temp;
 	}
 
+	// Searches the given integer array for the occurrence of the value.
+	// If the value exists in the array, returns first index of occurrence of
+	// the value. Otherwise, returns -1.
 	public static int findIndex(int[] array, int value) {
 		for (int index = 0; index < array.length; index++) {
 			if (array[index] == value) {
@@ -63,6 +72,9 @@ public class ArrayUtils {
 		return -1;
 	}
 
+	// Searches the given array for the occurrence of the value using the
+	// comparator passed. If the value exists in the array, returns first index
+	// of occurrence of the value. Otherwise, returns -1.
 	public static <T> int findIndex(T[] array, T value, Comparator<T> comparator) {
 		for (int index = 0; index < array.length; index++) {
 			if (comparator.compare(array[index], value) == 0) {
@@ -73,6 +85,7 @@ public class ArrayUtils {
 		return -1;
 	}
 
+	// Swaps the value in table[a][b] with the value in table[c][d].
 	public static void swap(Integer[][] table, int a, int b, int c, int d) {
 		Integer temp = table[a][b];
 		table[a][b] = table[c][d];
